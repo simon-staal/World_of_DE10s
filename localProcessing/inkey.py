cfff386be3798e31b3d9ef7a0b404d60a8f375e7
Simon Staal <sts219@ic.ac.uk>
2021-03-05 19:16:45 +0000

    trash

--- localProcessing/inkey.py
+++ localProcessing/inkey.py
@@ -1,9 +1,10 @@
 import msvcrt
 
 def main():
-    input = msvcrt.getch()
-    output = input.decode('utf-8')
-    print('Detected key: '+output)
+    while 1:
+        input = msvcrt.getch()
+        output = input.decode('utf-8')
+        print('Detected key: '+output)
 
 if __name__ == "__main__":
     main()
