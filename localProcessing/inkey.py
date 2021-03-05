2a749436770e804af5edb671cd39a5540d81824d
Simon Staal <sts219@ic.ac.uk>
2021-03-05 19:06:40 +0000

    Trying to make the script work

--- localProcessing/inkey.py
+++ localProcessing/inkey.py
@@ -1,16 +1,7 @@
 import msvcrt
 
-def getKey():
-    inkey = msvcrt.getch()
-    import sys
-    for i in range(sys.maxsize):
-        k=inkey()
-        if k!='': break
-
-    return k
-
 def main():
-    input = getKey()
+    input = msvcrt.getch()
     print(input)
 
 if __name__ == "__main__":
