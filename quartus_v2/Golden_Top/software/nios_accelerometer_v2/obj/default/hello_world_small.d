c69aa94a74efc77b9ea968f5e9d2f34d13d5573f
NDS19 <ndasilva115@gmail.com>
2021-03-07 16:39:08 +0000

    Quartus Qsys setup, all parts of the board implemented and Eclipse code should be working; used working lab 3 files as a base to work from

---
+++ quartus_v2/Golden_Top/software/nios_accelerometer_v2/obj/default/hello_world_small.d
@@ -0,0 +1,35 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ c\:/intelFPGA_lite/ip_labs/lab_3/Golden_Top/software/nios_accelerometer_v2_bsp/system.h \
+ c\:/intelFPGA_lite/ip_labs/lab_3/Golden_Top/software/nios_accelerometer_v2_bsp/linker.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_up_avalon_accelerometer_spi.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\alt_types.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\sys\alt_dev.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\sys\alt_llist.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\priv\alt_dev_llist.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_up_avalon_accelerometer_spi_regs.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\io.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\alt_types.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_avalon_timer_regs.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_avalon_timer.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\sys\alt_warning.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\sys\alt_irq.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\nios2.h \
+ c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\priv\alt_legacy_irq.h
+c\:/intelFPGA_lite/ip_labs/lab_3/Golden_Top/software/nios_accelerometer_v2_bsp/system.h:
+c\:/intelFPGA_lite/ip_labs/lab_3/Golden_Top/software/nios_accelerometer_v2_bsp/linker.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_up_avalon_accelerometer_spi.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\alt_types.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\sys\alt_dev.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\sys\alt_llist.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\priv\alt_dev_llist.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_up_avalon_accelerometer_spi_regs.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\io.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\alt_types.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_avalon_timer_regs.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_avalon_timer.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\sys\alt_warning.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\drivers\inc\altera_avalon_pio_regs.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\sys\alt_irq.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\nios2.h:
+c\:\intelfpga_lite\ip_labs\lab_3\golden_top\software\nios_accelerometer_v2_bsp\hal\inc\priv\alt_legacy_irq.h:
