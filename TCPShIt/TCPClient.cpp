1bf0f25c0905f8000a15d7e25ed4c1b7b443791f
kc319 <kc319@ic.ac.uk>
2021-03-10 14:03:45 +0000

    updates

--- TCPShIt/TCPClient.cpp
+++ TCPShIt/TCPClient.cpp
@@ -22,6 +22,8 @@ int main(int argc, char const *argv[])
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr = 82.69.42.98;
+
 
     // Convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
--- TCPShIt/TCPClientC.c
+++ TCPShIt/TCPClientC.c
@@ -12,17 +12,20 @@ int main(int argc, char const *argv[])
     struct sockaddr_in serv_addr;
     char *hello = "Hello from client";
     char buffer[1024] = {0};
+    char *serverip = "serveriphere";
+
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
         return -1;
     }
 
+
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET, serverip , &serv_addr.sin_addr)<=0)
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
@@ -38,4 +41,4 @@ int main(int argc, char const *argv[])
     valread = read( sock , buffer, 1024);
     printf("%s\n",buffer );
     return 0;
-} 
+}
